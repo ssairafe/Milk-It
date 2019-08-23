e5cfa13848eb97bc44488b4f43648ce16bf06260
ssairafe <sariaphd@gmail.com>
2019-08-23 16:01:42 -0700

    access products backend complete

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,5 +1,5 @@
 <?php
-
+startup();
 
 require_once ('db_connection.php');
 
@@ -7,7 +7,23 @@ require_once ('db_connection.php');
    die("Error:". mysqli_connect_error());
  }
 
+$query = "SELECT * FROM `products`";
+$result = mysqli_query($conn, $query);
 
+if (!$result){
+  throw new Exception('ERROR'.mysqli_error(''));
+}
 
+$output = [];
 
+while ($row = $result->fetch_assoc()){
+  $output[] = $row;
+}
+
+$output = json_encode($output);
+print($output);
+
+function startup(){
+  header("Content-type:application/json");
+}
 ?>
