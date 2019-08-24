9d51f95299c708ffe67fb89da28ac2bcd794c1e1
ssairafe <sariaphd@gmail.com>
2019-08-23 17:04:02 -0700

    id selected and data retrieved

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -7,7 +7,16 @@ require_once ('db_connection.php');
    die("Error:". mysqli_connect_error());
  }
 
+if (empty($_GET['id'])) {
+  $whereClause = '';
+} else if(is_numeric($_GET['id'])) {
+  $whereClause = "WHERE id = {$_GET['id']}";
+} else{
+  die('Non-Numeric');
+}
+
 $query = "SELECT * FROM `products`";
+$query = $query . $whereClause;
 $result = mysqli_query($conn, $query);
 
 if (!$result){
@@ -18,6 +27,10 @@ $output = [];
 
 while ($row = $result->fetch_assoc()){
   $output[] = $row;
+}
+
+if($output === [] and is_numeric($_GET['id'])){
+  $output[] = "invalid id";
 }
 
 $output = json_encode($output);
