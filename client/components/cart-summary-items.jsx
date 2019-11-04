44f3bb6e45abf362aeb81637b2c1318e23c22185
ssairafe <sariaphd@gmail.com>
2019-11-03 20:23:17 -0800

    fixed mobile responsiveness for counter buttons added new font styling added feature for added to cart messege upon clicking add to cart

--- client/components/cart-summary-items.jsx
+++ client/components/cart-summary-items.jsx
@@ -6,15 +6,15 @@ function CartSummaryItems(props) {
     let sum = item.price * item.count;
     grandTotal = grandTotal + sum;
     return (
-      <div className="row" key={item.id}>
+      <div className="cartProduct row" key={item.id}>
         <div style={{ 'textAlign': 'center' }} className="m-2 col-md">
-          <img style={{ 'height': '12rem', 'width': '16rem' }} src={item.image}/>
+          <img className="cartProductImage" style={{ 'height': '12rem', 'width': '16rem' }} src={item.image}/>
         </div>
         <div className="col-md">
-          <div className="m-2 row">{item.name}</div>
-          <div className="m-2 row">Quantity: {item.count}</div>
-          <div className="m-2 row">{('$' + (item.price / 100).toFixed(2))}</div>
-          <div className="m-2 row">{item.shortDescription}</div>
+          <div className="cartProductInfo m-2 row">{item.name}</div>
+          <div className="cartProductInfo m-2 row">Quantity: {item.count}</div>
+          <div className="cartProductInfo m-2 row">{('$' + (item.price / 100).toFixed(2))}</div>
+          <div className="cartProductInfo m-2 row">{item.shortDescription}</div>
           <div className="m-2 row removeButton" onClick={() => {
             props.delete(item);
           }}>Remove Item</div>
@@ -23,45 +23,64 @@ function CartSummaryItems(props) {
     );
   });
 
-  return (
-    <>
-      <a onClick={() => {
+  if (grandTotal === 0) {
+    return (
+      <>
+      <a className="backToCatalog" onClick={() => {
         props.view('catalog', {});
-      }} href="#" style={{ fontFamily: 'Lobster, cursive', fontSize: '2rem', marginLeft: '1rem', marginTop: '3rem', color: 'black', border: 'none' }} >&#60;- Back to Catalog
+      }} href="#" style={{ fontFamily: 'Lobster, cursive', marginLeft: '1rem', marginTop: '3rem', color: 'black', border: 'none' }} >&#60;- Back to Catalog
       </a>
-      <div className="container-fluid">
+      <div className="container-fluid" style={{ marginTop: '15%' }}>
         <div style={{ 'fontFamily': 'Lobster, cursive' }} className="row wickedCart">
           <div className="col"></div>
-          <div className="col">Your Milk</div>
+          <div className="cartTitle col">Your Cart Is Empty</div>
           <div className="col" >
-            <a onClick={() => {
-              props.clear();
-              props.refreshCart([]);
-            }} href="#" style={{ 'backgroundColor': 'black', 'color': 'white', border: 'none', marginLeft: '1rem', marginTop: '1rem' }} className="btn btn-primary mb-3"> Clear Cart
-            </a>
           </div>
         </div>
       </div>
-      <div className="container">
-        {renderingItems}
-        <div className="row">
-          <div className="col-md"></div>
-          <div className="col-md"></div>
-          <div className="col-md">
-            <div className="row">
-              Your Total: {('$' + (grandTotal / 100).toFixed(2))}
-            </div>
-            <div className="row">
+      </>
+    );
+  } else {
+    return (
+      <>
+        <a className="backToCatalog" onClick={() => {
+          props.view('catalog', {});
+        }} href="#" style={{ fontFamily: 'Lobster, cursive', marginLeft: '1rem', marginTop: '3rem', color: 'black', border: 'none' }} >&#60;- Back to Catalog
+        </a>
+        <div className="container-fluid">
+          <div style={{ 'fontFamily': 'Lobster, cursive' }} className="row wickedCart">
+            <div className="col"></div>
+            <div className="cartTitle col">Your Milk</div>
+            <div className="col" >
               <a onClick={() => {
-                props.view('checkout', {});
-              }} href="#" style={{ 'backgroundColor': 'black', 'color': 'white', 'float': 'right', border: 'none' }} className="btn btn-primary mb-3">Checkout
+                props.clear();
+                props.refreshCart([]);
+              }} href="#" style={{ 'backgroundColor': 'black', 'color': 'white', border: 'none', marginLeft: '1rem', marginTop: '1rem' }} className="btn btn-primary mb-3"> Clear Cart
               </a>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+        <div className="container">
+          {renderingItems}
+          <div className="row">
+            <div className="col-md"></div>
+            <div className="col-md"></div>
+            <div className="col-md">
+              <div className="cartTotal row">
+                Your Total: {('$' + (grandTotal / 100).toFixed(2))}
+              </div>
+              <div className="row">
+                <a onClick={() => {
+                  props.view('checkout', {});
+                }} href="#" style={{ 'backgroundColor': 'black', 'color': 'white', 'float': 'right', border: 'none' }} className="btn btn-primary mb-3">Checkout
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default CartSummaryItems;
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -3,7 +3,8 @@ class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: null
+      product: null,
+      addedToCart: false
     };
     this.getCountMinus = this.getCountMinus.bind(this);
     this.getCountPlus = this.getCountPlus.bind(this);
@@ -52,32 +53,59 @@ class ProductDetails extends React.Component {
     }
     return (
       <>
-        <a onClick={() => {
+        <a className="backToCatalog" onClick={() => {
           this.props.view('catalog', { id: this.state.product });
-        }} href="#" style={{ fontFamily: 'Lobster, cursive', fontSize: '2rem', marginLeft: '1rem', marginTop: '3rem', color: 'black', border: 'none' }} >&#60;- Back to Catalog
+        }} href="#" style={{ fontFamily: 'Lobster, cursive', marginLeft: '1rem', marginTop: '3rem', color: 'black', border: 'none' }} >&#60;- Back to Catalog
         </a>
         <div className="container">
           <div style={{ display: 'inline-block' }}>
-            <img style={{ 'width': '20rem' }} image={this.state.product.image} src={this.state.product.image}/>
+            <img className='prodInfoImage' style={{ 'width': '20rem' }} image={this.state.product.image} src={this.state.product.image}/>
           </div>
           <div className="detailsBackground" style={{ width: '40%', marginLeft: '10%', display: 'inline-block', verticalAlign: 'bottom' }}>
-            <h1>{this.state.product.name}</h1>
+            <h1 className="productName">{this.state.product.name}</h1>
             <h2>{'$' + (this.state.product.price / 100).toFixed(2)}</h2>
             {this.state.product.shortDescription}
             <div>
               <button onClick={() => {
                 this.props.add(this.state.product);
+                this.setState({
+                  addedToCart: true
+                });
+                setTimeout(() => {
+                  this.setState({
+                    addedToCart: false
+                  });
+                }, 1000);
               }} href="#" className="btn btn-primary mb-4" style={{ border: 'none', backgroundColor: 'black', marginTop: '5%' }}>Add to Cart
               </button>
-              <div style={{ position: 'relative', display: 'inline-block', marginLeft: '10%' }}>
+              {this.state.product.name === 'Lioness-Milk' ? <div id="lionCounter" style={{ position: 'absolute', display: 'inline-block', marginLeft: '10%' }}>
                 <div style={{ position: 'relative', display: 'inline-block' }}>
-                  <h3 onClick={() => { this.getCountPlus(); }} style={{ position: 'relative', display: 'inline-block', transform: 'rotate(180deg)', fontSize: '2rem', top: '15px', cursor: 'pointer' }}>v</h3>
-                  <h3 onClick={() => { this.getCountMinus(); }} style={{ position: 'relative', display: 'block', fontSize: '2rem', cursor: 'pointer' }}>v</h3>
+                  <h3 className="countButton" onClick={() => { this.getCountPlus(); }} style={{ position: 'relative', display: 'inline-block', transform: 'rotate(180deg)', top: '15px', cursor: 'pointer' }}>v</h3>
+                  <h3 className="countButton" onClick={() => { this.getCountMinus(); }} style={{ position: 'relative', display: 'block', cursor: 'pointer' }}>v</h3>
                 </div>
-                <div style={{ position: 'relative', display: 'inline-block', left: '20%', fontSize: '3rem' }}>
+                <div id="productCounter" style={{ position: 'relative', display: 'inline-block', left: '5%' }}>
                   {this.state.product.counter}
                 </div>
-              </div>
+              </div> : this.state.product.name === 'Goat-Milk'
+                ? <div id="goatCounter" style={{ position: 'absolute', display: 'inline-block', marginLeft: '10%' }}>
+                  <div style={{ position: 'relative', display: 'inline-block' }}>
+                    <h3 className="countButton" onClick={() => { this.getCountPlus(); }} style={{ position: 'relative', display: 'inline-block', transform: 'rotate(180deg)', top: '15px', cursor: 'pointer' }}>v</h3>
+                    <h3 className="countButton" onClick={() => { this.getCountMinus(); }} style={{ position: 'relative', display: 'block', cursor: 'pointer' }}>v</h3>
+                  </div>
+                  <div id="productCounter" style={{ position: 'relative', display: 'inline-block', left: '5%' }}>
+                    {this.state.product.counter}
+                  </div>
+                </div>
+                : <div id="arrowHolder" style={{ position: 'absolute', display: 'inline-block', marginLeft: '10%' }}>
+                  <div style={{ position: 'relative', display: 'inline-block' }}>
+                    <h3 className="countButton" onClick={() => { this.getCountPlus(); }} style={{ position: 'relative', display: 'inline-block', transform: 'rotate(180deg)', top: '15px', cursor: 'pointer' }}>v</h3>
+                    <h3 className="countButton" onClick={() => { this.getCountMinus(); }} style={{ position: 'relative', display: 'block', cursor: 'pointer' }}>v</h3>
+                  </div>
+                  <div id="productCounter" style={{ position: 'relative', display: 'inline-block', left: '5%' }}>
+                    {this.state.product.counter}
+                  </div>
+                </div>}
+              {this.state.addedToCart === true ? <div style={{ position: 'absolute', fontFamily: 'Staatliches, cursive', fontSize: '2rem' }}>Added to Cart!</div> : ''}
             </div>
           </div>
         </div>
@@ -87,12 +115,9 @@ class ProductDetails extends React.Component {
             <div className="prodDetails">{this.state.product.longDescription}</div>
           </div>
         </div>
-
       </>
-
     );
   }
-
 }
 
 export default ProductDetails;
